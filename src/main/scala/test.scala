21a7b0de1a8b09eac7ccf604a046b450a0f5cac3
dyzsasd <dyzsasd@hotmail.com>
2014-11-04 11:57:58 +0100

    commit avant weekend

--- src/main/scala/test.scala
+++ src/main/scala/test.scala
@@ -1,5 +1,8 @@
 import org.apache.spark.{HashPartitioner, SparkContext, SparkConf}
 import org.apache.spark.SparkContext._
+import scala.math.random
+import org.apache.spark._
+
 
 object SimpleApp {
   def main(args: Array[String]) {
@@ -25,5 +28,21 @@ object SimpleApp {
     },preservesPartitioning = true)
     val res = transrdd.collect()
       res.foreach(println)
+  }
+}
+
+object SparkPi {
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("Spark Pi").setMaster("spark://cluster3:7077")
+    val spark = new SparkContext(conf)
+    val slices = if (args.length > 0) args(0).toInt else 2
+    val n = 100000 * slices
+    val count = spark.parallelize(1 to n, slices).map { i =>
+      val x = random * 2 - 1
+      val y = random * 2 - 1
+      if (x*x + y*y < 1) 1 else 0
+    }.reduce(_ + _)
+    println("Pi is roughly " + 4.0 * count / n)
+    spark.stop()
   }
 }
