6fd5de24e90d0c09e4e37d1d9a2c8383d5fffc21
dyzsasd <dyzsasd@hotmail.com>
2014-11-05 12:43:32 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	pom.xml

--- src/main/scala/test.scala
+++ src/main/scala/test.scala
@@ -3,18 +3,34 @@ import org.apache.spark.SparkContext._
 import scala.math.random
 import org.apache.spark._
 
-object SparkPi {
+object InverseOrder {
   def main(args: Array[String]) {
-    val conf = new SparkConf().setAppName("Spark Pi").setMaster("spark://cluster3:7077")
-    val spark = new SparkContext(conf)
-    val slices = if (args.length > 0) args(0).toInt else 2
-    val n = 100000 * slices
-    val count = spark.parallelize(1 to n, slices).map { i =>
-      val x = random * 2 - 1
-      val y = random * 2 - 1
-      if (x*x + y*y < 1) 1 else 0
-    }.reduce(_ + _)
-    println("Pi is roughly " + 4.0 * count / n)
-    spark.stop()
+    val conf = new SparkConf()
+
+    val sc = new SparkContext(conf)
+
+    var path = ""
+    if(args.length>0){
+      path = args(0)
+    }else{
+      path = "hdfs:///user/szhang/pubparser/resources/outputtest.csv"
+    }
+    val rawData = sc.textFile(path)
+
+    val indexedData = rawData.zipWithIndex()
+
+    val endedData = indexedData.map(x=>{
+      val text = x._1
+      var restext:String = text
+      if (text.split("\t").length==3){
+        restext = text+"endline"
+      }
+
+      (x._2, restext)
+    })
+
+    val inversData = endedData.sortByKey(ascending = false,numPartitions = 20)
+
+    inversData.map(x=>(x._1+"\t"+x._2)).saveAsTextFile("hdfs:///user/szhang/pubparser/resources/indexed")
   }
 }
