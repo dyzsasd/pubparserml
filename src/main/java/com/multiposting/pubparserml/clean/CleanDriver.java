1413050b08cc2394a21cb87221475c4228621315
dyzsasd <dyzsasd@hotmail.com>
2014-11-05 14:16:08 +0100

    add mapreduce

--- src/main/java/com/multiposting/pubparserml/clean/CleanDriver.java
+++ src/main/java/com/multiposting/pubparserml/clean/CleanDriver.java
@@ -31,7 +31,6 @@ class CleanDriver extends Configured implements Tool{
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
         job.setMapperClass(CleanMapper.class);
-        job.setReducerClass(CleanReducer.class);
 
         job.setNumReduceTasks(0);
 
--- src/main/java/com/multiposting/pubparserml/clean/CleanMapper.java
+++ src/main/java/com/multiposting/pubparserml/clean/CleanMapper.java
@@ -15,7 +15,11 @@ public class CleanMapper extends Mapper<LongWritable, Text, Text, NullWritable> 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String data = value.toString();
-        data.replace("\n"," ");
+        data = data.replaceAll("\t","@tab");
+        data = data.replaceAll("\\\\n"," ");
+        data = data.replace("[^\\p{L}]+"," ");
+        data = data.replaceAll("@tab","\t");
+        context.write(new Text(data),NullWritable.get());
 
     }
 }
